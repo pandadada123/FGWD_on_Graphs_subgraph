c17192e004410321ff7adea76a9bd4d8ca123555
pandadada <pandadadawx@gmail.com>
2023-04-14 10:15:45 +0200

    v1.0
    include Peyres trick, and simple successful example of entropic

---
+++ examples/subgraph_2comm.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 24 16:49:52 2023
+
+@author: Pandadada
+"""
+
+# Finding subgraph
+
+import numpy as np
+import os,sys
+
+# sys.path.append(os.path.realpath('../lib'))
+sys.path.append(os.path.realpath('E:/Master Thesis/FGWD_on_Graphs_subgraph/lib_1.0'))
+
+from graph import graph_colors,draw_rel,draw_transp,Graph,wl_labeling
+from ot_distances import Fused_Gromov_Wasserstein_distance,Wasserstein_distance
+import copy
+# from data_loader import load_local_data,histog,build_noisy_circular_graph
+import matplotlib.pyplot as plt
+import networkx as nx
+import ot
+
+'''G is Graph object
+   g is nx_graph
+'''
+G11=Graph() # community 1
+G11.add_attributes({0:1,1:7,2:5,3:3,4:9})    # add color to nodes
+G11.add_edge((0,1))
+G11.add_edge((1,2))
+G11.add_edge((2,3))
+G11.add_edge((3,4))
+G11.add_edge((4,0))
+# g1.add_edge((0,2))
+# g1.add_edge((1,3))
+G11.add_edge((0,2))
+G11.add_edge((0,3))
+G11.add_edge((2,4))
+G11.add_edge((1,3))
+G11.add_edge((1,4))
+
+G12=Graph() # community 2
+G12.add_attributes({0:1,1:7,2:5,3:3,4:9})
+G12.add_edge((0,3))
+G12.add_edge((1,3))
+G12.add_edge((2,3))
+G12.add_edge((4,3))
+
+# N=5 # five different colors/nodes in each subgra
+# mu1=-1.5
+# mu2=1.5
+vmin=0
+vmax=9  # the range of color
+# np.random.seed(12)
+# g1=build_comunity_graph(N=N,mu=mu1,sigma=0.8,pw=0.5)
+# g2=build_comunity_graph(N=N,mu=mu2,sigma=0.8,pw=0.5)
+
+
+def merge_graph(g1,g2):  # inputs are nx_graph
+    gprime=nx.Graph(g1)
+    N0=len(gprime.nodes())
+    g2relabel=nx.relabel_nodes(g2, lambda x: x +N0)
+    gprime.add_nodes_from(g2relabel.nodes(data=True))
+    gprime.add_edges_from(g2relabel.edges(data=True)) 
+    gprime.add_edge(N0-1,N0)
+    
+    return gprime
+
+G1 = Graph ( merge_graph(G11.nx_graph,G12.nx_graph) )  # Graph including 2 communities
+G2_nodummy = copy.deepcopy(G11) # version without dummy node
+G2 = copy.deepcopy(G11)
+G2.add_attributes({5:1}) # add a dummy node to G2
+
+#%%  The followings are fixed
+g1 = G1.nx_graph
+g2 = G2.nx_graph
+
+plt.figure(figsize=(8,5))
+draw_rel(g1,vmin=vmin,vmax=vmax,with_labels=True,draw=False)
+draw_rel(g2,vmin=vmin,vmax=vmax,with_labels=True,shiftx=3,draw=False)
+plt.title('Two graphs. Color indicates the label')
+plt.show()
+
+#%% weights and feature metric
+p1=ot.unif(len(G1.nodes()))
+p2_nodummy=1/len(G1.nodes()) * np.ones([len(G2_nodummy.nodes())])    # ACTUALLY NOT USED IN THE ALGORITHM
+p2=np.append(p2_nodummy,1-sum(p2_nodummy))
+
+# fea_metric = 'dirac'
+# fea_metric = 'hamming'
+fea_metric = 'sqeuclidean'
+
+#%%
+thresh=0.004
+# WD
+fig=plt.figure(figsize=(10,8))
+dw,transp_WD=Wasserstein_distance(features_metric=fea_metric).graph_d(G1,G2,p1,p2)
+# dw,log_WD,transp_WD=Fused_Gromov_Wasserstein_distance(alpha=0,features_metric=fea_metric,method='shortest_path',loss_fun= 'square_loss').graph_d(G1,G2,p1,p2,p2_nodummy)
+plt.title('WD coupling')
+draw_transp(G1,G2,transp_WD,shiftx=2,shifty=0.5,thresh=thresh,swipy=True,swipx=False,with_labels=True,vmin=vmin,vmax=vmax)
+plt.show()
+
+# GWD
+fig=plt.figure(figsize=(10,8))
+dgw,log_GWD,transp_GWD=Fused_Gromov_Wasserstein_distance(alpha=1,features_metric=fea_metric,method='shortest_path',loss_fun= 'square_loss').graph_d(G1,G2,p1,p2,p2_nodummy)
+plt.title('GWD coupling')
+draw_transp(G1,G2,transp_GWD,shiftx=2,shifty=0.5,thresh=thresh,swipy=True,swipx=False,with_labels=True,vmin=vmin,vmax=vmax)
+plt.show()
+
+# FGWD
+alpha=0.99
+fig=plt.figure(figsize=(10,8))
+dfgw,log_FGWD,transp_FGWD=Fused_Gromov_Wasserstein_distance(alpha=alpha,features_metric=fea_metric,method='shortest_path',loss_fun= 'square_loss').graph_d(G1,G2,p1,p2,p2_nodummy)
+plt.title('FGWD coupling')
+draw_transp(G1,G2,transp_FGWD,shiftx=2,shifty=0.5,thresh=thresh,swipy=True,swipx=False,with_labels=True,vmin=vmin,vmax=vmax)
+plt.show()
+
+#%% FGWD, find alpha
+alld=[]
+x=np.linspace(0,1,10)
+for alpha in x:
+    d,log,transp=Fused_Gromov_Wasserstein_distance(alpha=alpha,features_metric=fea_metric).graph_d(G1,G2,p1,p2,p2_nodummy)
+    alld.append(d)
+plt.plot(x,alld)
+plt.title('Evolution of FGW dist in wrt alpha \n max={}'.format(x[np.argmax(alld)]))
+plt.xlabel('Alpha')
+plt.xlabel('FGW dist')
+plt.show()
+
+# optimal matching
+fig=plt.figure(figsize=(10,8))
+thresh=0.004
+alpha_opt=x [ alld.index(max(alld)) ]
+dfgw_opt,log_FGWD_opt,transp_FGWD_opt=Fused_Gromov_Wasserstein_distance(alpha=alpha_opt,features_metric=fea_metric).graph_d(G1,G2,p1,p2,p2_nodummy)
+# d=dfgw.graph_d(g1,g2)
+# plt.title('FGW coupling, dist : '+str(np.round(dfgw,3)),fontsize=15)
+plt.title('FGW coupling, alpha = opt')
+draw_transp(G1,G2,transp_FGWD_opt,shiftx=2,shifty=0.5,thresh=thresh,
+            swipy=True,swipx=False,with_labels=True,vmin=vmin,vmax=vmax)
+plt.show()
+
+# print('Wasserstein distance={}, Gromov distance={} \nFused Gromov-Wasserstein distance for alpha {} = {}'.format(dw,dgw,alpha,dfgw))
+
---
+++ lib_1.0/ot_distances.py
@@ -0,0 +1,215 @@
+import ot
+import FGW as fgw
+import numpy as np
+import time
+from graph import NoAttrMatrix
+from utils import hamming_dist
+import networkx as nx
+"""
+The following classes adapt the OT distances to Graph objects
+"""
+
+class BadParameters(Exception):
+    pass
+
+class Wasserstein_distance():
+    """ Wasserstein_distance is a class used to compute the Wasserstein distance between features of the graphs.
+    
+    Attributes
+    ----------    
+    features_metric : string
+                      The name of the method used to compute the cost matrix between the features
+    transp : ndarray, shape (ns,nt) 
+           The transport matrix between the source distribution and the target distribution 
+    """
+    def __init__(self,features_metric='sqeuclidean'): #remplacer method par distance_method  
+        self.features_metric=features_metric
+        self.transp=None
+
+    def reshaper(self,x):
+        x=np.array(x)
+        try:
+            a=x.shape[1]
+            return x
+        except IndexError:
+            return x.reshape(-1,1)
+
+    def graph_d(self,graph1,graph2,t1masses,t2masses):
+        """ Compute the Wasserstein distance between two graphs. Uniform weights are used.        
+        Parameters
+        ----------
+        graph1 : a Graph object
+        graph2 : a Graph object
+        Returns
+        -------
+        The Wasserstein distance between the features of graph1 and graph2
+        """
+
+        nodes1=graph1.nodes()  # nodes of two graphs
+        nodes2=graph2.nodes()
+        # t1masses = np.ones(len(nodes1))/len(nodes1)  # uniform weights
+        # t2masses = np.ones(len(nodes2))/len(nodes2)
+        x1=self.reshaper(graph1.all_matrix_attr())
+        x2=self.reshaper(graph2.all_matrix_attr())
+
+        if self.features_metric=='dirac':
+            # f=lambda x,y: x!=y   # x not equal to y
+            f=lambda x,y: int(x!=y)   # x not equal to y
+            M=ot.dist(x1,x2,metric=f)
+        else:
+            M=ot.dist(x1,x2,metric=self.features_metric) 
+        if np.max(M)!=0:
+            M= M/np.max(M)
+        self.M=M
+
+        transp = ot.emd(t1masses,t2masses, M)
+        self.transp=transp
+
+        return np.sum(transp*M), transp
+
+    def get_tuning_params(self):
+        return {"features_metric":self.features_metric}
+
+
+
+class Fused_Gromov_Wasserstein_distance():
+    """ Fused_Gromov_Wasserstein_distance is a class used to compute the Fused Gromov-Wasserstein distance between graphs 
+    as presented in [3]
+    
+    Attributes
+    ----------  
+    alpha : float 
+            The alpha parameter of FGW
+    method : string
+             The name of the method used to compute the structures matrices of the graphs. See Graph class
+    max_iter : integer
+               Number of iteration of the FW algorithm for the computation of FGW.
+    features_metric : string
+                      The name of the method used to compute the cost matrix between the features
+                      For hamming_dist see experimental setup in [3]
+    transp : ndarray, shape (ns,nt) 
+           The transport matrix between the source distribution and the target distribution
+    amijo : bool, optionnal
+            If True the steps of the line-search is found via an amijo research. Else closed form is used.  
+            If there is convergence issues use False.
+    References
+    ----------
+    .. [3] Vayer Titouan, Chapel Laetitia, Flamary R{\'e}mi, Tavenard Romain
+          and Courty Nicolas
+        "Optimal Transport for structured data with application on graphs"
+        International Conference on Machine Learning (ICML). 2019.
+    """
+
+    def __init__(self,alpha=0.5,method='shortest_path',features_metric='sqeuclidean',loss_fun ='square_loss', max_iter=500,verbose=False,amijo=True): #remplacer method par distance_method  
+        self.method=method
+        self.max_iter=max_iter
+        self.alpha=alpha
+        self.features_metric=features_metric
+        self.transp=None
+        self.log=None
+        self.verbose=verbose
+        self.amijo=amijo
+        #if alpha==0 or alpha==1:
+        #    self.amijo=True
+        self.loss_fun=loss_fun
+
+    def reshaper(self,x):
+        try:
+            a=x.shape[1]
+            return x
+        except IndexError:
+            return x.reshape(-1,1)
+
+    def calc_fgw(self,M,C1,C2,C2_nodummy,t1masses,t2masses,p2_nodummy):
+        transpwgw,log= fgw.fgw_lp((1-self.alpha)*M,C1,C2,C2_nodummy,t1masses,t2masses,p2_nodummy,self.loss_fun,G0=None,alpha=self.alpha,verbose=self.verbose,amijo=self.amijo,log=True)      
+        return transpwgw,log
+        
+    def graph_d(self,graph1,graph2,t1masses,t2masses,p2_nodummy):
+        """ Compute the Fused Gromov-Wasserstein distance between two graphs. Uniform weights are used.        
+        Parameters
+        ----------
+        graph1 : a Graph object
+        graph2 : a Graph object
+        Returns
+        -------
+        The Fused Gromov-Wasserstein distance between the features of graph1 and graph2
+        """
+        gofeature=True
+        nodes1=graph1.nodes()
+        nodes2=graph2.nodes()
+        startstruct=time.time()
+        
+        n1=len(nodes1)
+        n2=len(nodes2)
+        
+        # C1=graph1.distance_matrix(method=self.method)
+        C1 = np.zeros ([n1,n1])
+        for i in range(n1):
+            for ii in range(n1):  
+                C1[i][ii] = nx.shortest_path_length(graph1.nx_graph,source=i,target=ii)
+                
+        
+        # C2=graph2.distance_matrix(method=self.method)
+        C2_nodummy = np.zeros ([n2-1,n2-1])
+        for j in range(n2-1):
+            for jj in range(n2-1):  
+                C2_nodummy[j][jj] = nx.shortest_path_length(graph2.nx_graph,source=j,target=jj)
+        
+        # C2_shape=np.shape(C2)        
+        # C2_nodummy=C2[:,0:C2_shape[1]-1]
+        # C2_nodummy=C2_nodummy[0:C2_shape[0]-1,:]
+        
+        C2_temp=np.append(C2_nodummy,100*np.ones([n2-1,1]),axis=1)
+        C2=np.append(C2_temp,100*np.ones([1,n2]),axis=0)
+        C2[n2-1,n2-1]=0
+
+        
+        end2=time.time()
+        # t1masses = np.ones(len(nodes1))/len(nodes1)
+        # t2masses = np.ones(len(nodes2))/len(nodes2)  # uniform weights
+        try :
+            x1=self.reshaper(graph1.all_matrix_attr())
+            x2=self.reshaper(graph2.all_matrix_attr())
+        except NoAttrMatrix:
+            x1=None
+            x2=None
+            gofeature=False
+        if gofeature : 
+            if self.features_metric=='dirac': # (does not work) Dirac is the same as hamming_dist for scalar comparison
+                # f=lambda x,y: x!=y
+                f=lambda x,y: int(x!=y)
+                M=ot.dist(x1,x2,metric=f)
+            elif self.features_metric=='hamming': #see experimental setup in the original paper
+                # f=lambda x,y: hamming_dist(x,y)
+                # M=ot.dist(x1,x2,metric=f)
+                M=ot.dist(x1,x2,metric='hamming')
+            elif self.features_metric=='sqeuclidean':
+                M=np.zeros((C1.shape[0],C2.shape[0]))
+                for i in range(C1.shape[0]):
+                    for j in range(C2.shape[0]):  
+                          M[i][j]=sum(pow(x1[i,:]-x2[j,:] ,2))
+                
+            else:
+                M=ot.dist(x1,x2,metric=self.features_metric)
+                
+            self.M=M
+        else:
+            M=np.zeros((C1.shape[0],C2.shape[0]))
+
+        startdist=time.time()
+        transpwgw,log=self.calc_fgw(M,C1,C2,C2_nodummy,t1masses,t2masses,p2_nodummy) #return the transport matrix and FGWD value
+        enddist=time.time()
+
+        enddist=time.time()
+        log['struct_time']=(end2-startstruct)
+        log['dist_time']=(enddist-startdist)
+        self.transp=transpwgw
+        self.log=log
+
+        # return log['loss','delta_fval','gradient', 'descent'][::-1][0],transpwgw  # the loss value in the last iteration is the returned value
+        return log['loss'][::-1][0],log,transpwgw
+
+    def get_tuning_params(self):
+        """Parameters that defined the FGW distance """
+        return {"method":self.method,"max_iter":self.max_iter,"alpha":self.alpha,
+        "features_metric":self.features_metric,"amijo":self.amijo}
